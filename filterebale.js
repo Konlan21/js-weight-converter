a53bff966a2f6c19815dbf65df424efbcd402c15
Konlan21 <joekonlan16@gmail.com>
2024-02-25 16:35:50 +0000

    Configure visible on input

--- filterebale.js
+++ filterebale.js
@@ -3,20 +3,20 @@ let input = document.getElementById('input');
 let gramsOutput = document.getElementById('gramsOutput');
 let kgOutput = document.getElementById('kgOutput');
 let ozOutput = document.getElementById('ozOutput');
-console.log(kgOutput);
-
+let output = (document.getElementById("output").style.visibility = "hidden");
+console.log();
 
 function convert() {
-    const weightLbs = input.value
-    let weightIngrams = weightLbs * 453.592
-    gramsOutput.innerText = weightIngrams
+  let output = (document.getElementById("output").style.visibility = "visible");
+  const weightLbs = input.value;
+  let weightIngrams = weightLbs * 453.592;
+  gramsOutput.innerText = weightIngrams;
 
-    let weightInKg = weightLbs * 0.453592;
-    kgOutput.innerText = weightInKg
+  let weightInKg = weightLbs * 0.453592;
+  kgOutput.innerText = weightInKg;
 
-    let weightInOz = weightLbs * 16;
-    ozOutput.innerText = weightInOz;
-
+  let weightInOz = weightLbs * 16;
+  ozOutput.innerText = weightInOz;
 }
 
 
